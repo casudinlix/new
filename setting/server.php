9b0348d9cce487b33f9679cb5c1063d45189809a
“casudinlix” <casudin@programmer.net>
2015-12-19 15:23:27 +0700

    baru

---
+++ setting/server.php
@@ -0,0 +1,14 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname = "localhost";
+$database = "tiket";
+$username = "cas";
+$password = "bintang";
+$connect = mysqli_connect($hostname,$username,$password,$database);
+if (!$connect) {
+	mysqli_report("error")
+}
+
+?>
